a9b260034e3151fee7c3c9fcf928d0d295d7bedb
Ross Gardler <ross@gardler.org>
2015-10-23 23:52:30 +0000

    install some essential packages

--- configure.sh
+++ configure.sh
@@ -1,3 +1,12 @@
 # Configure a Linux Machine
 
+sudo apt-get update
+sudo apt-get install -y ruby
+sudo apt-get install -y lynx
+sudo apt-get install -y vi
+sudo apt-get install -y emacs
+
+
+sudo gem install tmuxinator
+
 cp -Rf . ~
