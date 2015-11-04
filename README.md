bf35a4e8ad253252882cf13cf20d07017e06fe71
Ross Gardler <ross@gardler.org>
2015-11-04 00:09:34 -0800

    don't run as sudo as it makes some files root owned

--- README.md
+++ README.md
@@ -2,6 +2,5 @@ To configure a LInux machine....
 
 ```
 git clone https://github.com/rgardler/linux-config.git
-cd ./linux-config
-sudo configure.sh
+./linux-config/configure.sh
 ```
--- configure.sh
+++ configure.sh
@@ -6,20 +6,16 @@ DOCKER_COMPOSE_VERSION=1.5.0rc3
 
 # Preparation
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run this script as root using sudo"
-  exit
-fi
-
-apt-get update
+SCRIPT_DIR=$(cd "${0%/*}" 2>/dev/null; echo "$PWD"/"${0##*/}")
+sudo apt-get update
 
 # Programming
 
-apt-get install -y ruby
-apt-get install -y python-pip
-apt-get install -y lynx
-apt-get install -y vi
-apt-get install -y emacs
+sudo apt-get install -y ruby
+sudo apt-get install -y python-pip
+sudo apt-get install -y lynx
+sudo apt-get install -y vi
+sudo apt-get install -y emacs
 
 # Utilities
 
@@ -27,8 +23,10 @@ gem install tmuxinator
 
 # Docker
 
-curl -L https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-`uname -s`-`uname -m` > docker-compose
+sudo mv docker-compose /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+rm docker-compose
 
 # git 
 
